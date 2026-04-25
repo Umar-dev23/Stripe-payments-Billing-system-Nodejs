22325fa71f50fe4bb2865145702e04b809fc96d4
umar01-fabtechsol <umar.dev23@gmail.com>
2026-04-25 22:20:42 +0500

    created more files..

---
+++ src/modules/stripe/checkout/checkout.service.js
@@ -0,0 +1,11 @@
+import stripe from '../stripe.config';
+
+export const createSession = async (lineItems) => {
+  return await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: `${process.env.FRONTEND_URL}/success`,
+    cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+  });
+};
