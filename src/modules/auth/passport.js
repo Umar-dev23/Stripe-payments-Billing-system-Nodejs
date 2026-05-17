7765385d2d07cf6d14e1352ec3943e2c44164bcb
umar01-fabtechsol <umar.dev23@gmail.com>
2026-05-17 18:27:08 +0500

    Update passport.js

--- src/modules/auth/passport.js
+++ src/modules/auth/passport.js
@@ -77,7 +77,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://4ll9cl-3000.csb.app/api/auth/google/callback',
+      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
